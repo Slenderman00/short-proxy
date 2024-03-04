6141563c65f2da130b1a029a58211a68595c8991
Joar Heimonen <joarheimonen@live.no>
2024-03-04 05:14:43 +0100

    Initial commit 2

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,36 @@
 import express from 'express';
+import proxy from 'express-http-proxy'
 
 const dotenv = require('dotenv')
 
 const app = express();
+const PORT = (process.env.PORT as any) || 3000;
 
-app.get('/', (req, res) => {
+app.use('/', (req, res, next) => {
+    let segments = req.path.split('/');
+    const id = segments[1];
 
+    if(id.includes('api')) {
+        console.log("yeet")
+        next();
+        return;
+    }
+
+    segments = segments.slice(2);
+    let url = '/' + segments.join('/');
+    console.log(req.url)
+    proxy('grindr.mobi', {
+        parseReqBody: false,
+        proxyReqPathResolver: ((req) => {
+            return url;
+        }),
+    })(req, res, next);
 })
 
-app.post('/', (req, res) => {
-
+app.get('/api', (req, res) => {
+    res.send({'yeer': 'yeet'})
 })
+
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Is running at port: ${PORT}`)
+});
